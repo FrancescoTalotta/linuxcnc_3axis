ccefa6d820755f2a0b9e3e12b13a124501051910
ciccio <ciccio@localhost.localdomain>
2018-06-24 00:48:13 +0200

    added a new routine M111

--- python_post_processors/Z_tool_offset.py
+++ python_post_processors/Z_tool_offset.py
@@ -66,8 +66,7 @@ def main():
            print("  (MSG,Rimuovi Z Probe, e poi premi S)")
            print("  M0")
            print("O100 endsub")
-           print("(MSG, Set the machine to the tool probe position. If already set click press S to resume. If not set press ESC and set the machine to the desireded tool probe position, then run the program again.)")
-           print("M0")
+           print("M111")
            print("G28.1")
            print("M102")
 
